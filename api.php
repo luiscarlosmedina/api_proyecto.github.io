344c05011e79ec60351487744c293c7411f2c9eb
luismedina <luiscarlosmedina481@gmail.com>
2023-09-05 10:21:47 -0500

    version para cloud

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'controllerjson.php';
 
-header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 $apicall = isset($_GET['apicall']) ? $_GET['apicall'] : '';
--- database.php
+++ database.php
@@ -2,10 +2,10 @@
 class Database{
 
     public function getConnection(){
-		$localhost = 'localhost';
-		$database = 'asocivica';
-		$user = 'root';
-		$password = 'admin';
+		$localhost = 'b4zpdfljvwzlvh9yqpvc-mysql.services.clever-cloud.com:3306';
+		$database = 'b4zpdfljvwzlvh9yqpvc';
+		$user = 'ulcb8ojs5uz00guz';
+		$password = 'XdvZTIccbpKQJnzSlI3f';
 
 		$link = new PDO("mysql:host=$localhost;dbname=$database",$user,$password);
 
